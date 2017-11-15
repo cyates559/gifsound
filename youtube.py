180c0b179cb71740d947e60556fd34d35929b5c4
Carsen Yates <fatburgeriii@gmail.com>
2017-11-15 11:02:27 -0800

    added more url schemes to youtube.py

--- youtube.py
+++ youtube.py
@@ -8,13 +8,17 @@ import urllib.parse
 
 def get_embed_url(main_url):
 
+	id=None
 	# User gave us the url we are looking for, so we are done here.
 	if 'https://youtube.com/embed/' in main_url:
 		return main_url
 
 	# We need to extract the id from the url to produce the embed url
-	id=None
-	if 'youtube.com/watch?v=' in main_url:
+	elif 'youtu.be' in main_url:
+		r = urllib.parse.urlparse(main_url)
+		id = r.path.replace('/', '')
+
+	elif 'youtube.com/watch?v=' in main_url:
 		r = urllib.parse.urlparse(main_url)
 		id = urllib.parse.parse_qs(r.query)['v'][0]
 
@@ -23,5 +27,8 @@ def get_embed_url(main_url):
 
 	return f'https://youtube.com/embed/{id}'
 
+
+# Some tests, these should all print the same URL
+print(get_embed_url('https://www.youtu.be/8y7XGmORIXM?t=1m'))
 print(get_embed_url('https://www.youtube.com/watch?v=8y7XGmORIXM'))
 print(get_embed_url('https://www.youtube.com/watch?v=8y7XGmORIXM&start=1'))
