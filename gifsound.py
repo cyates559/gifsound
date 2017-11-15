4f678a647a0084a69faec26f2e86e1f875449f6b
Carsen Yates <fatburgeriii@gmail.com>
2017-11-15 10:24:27 -0800

    Added flask_bootstrap to flask app, created base layout file

--- gifsound.py
+++ gifsound.py
@@ -1,11 +1,18 @@
 #!pyenv/bin/python
+
+# AUTHORS: Erick Shaffer, Carsen Yates, Mimi Leon
+# DATE: 11/14/2017
+# PURPOSE: This file controls all the flask routes for gifsound
+
+
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-
+Bootstrap(app)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello World!'
 
 
