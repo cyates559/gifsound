08f4ee74a767796b8f6daea43060394fe4d7e9d6
Carsen Yates <fatburgeriii@gmail.com>
2017-12-10 15:11:33 -0800

    Doc outline

--- sh/create_tables.sh
+++ sh/create_tables.sh
@@ -1,1 +1,7 @@
+## COURSE: CST 205 - Multimedia Design & Programming
+## TITLE: create_tables.sh
+## ABSTRACT: 
+## AUTHORS: Erick Shaffer
+## DATE:
+
 curl -X POST 127.0.0.1:5000/api/create_tables/somekeyzz
