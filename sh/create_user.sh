c2258bdb94eba202cc0ffb66b2ad81fa51eb7f59
Carsen Yates <fatburgeriii@gmail.com>
2017-12-10 14:30:25 -0800

    Doc outline

--- sh/create_user.sh
+++ sh/create_user.sh
@@ -1,2 +1,8 @@
+## COURSE: CST 205 - Multimedia Design & Programming
+## TITLE: create_user.sh
+## ABSTRACT: 
+## AUTHORS: Erick Shaffer
+## DATE: 
+
 RANDOM=123
 curl -X GET 127.0.0.1:5000/api/register/username$RANDOM/email$RANDOM/password/3/somekeyzz
