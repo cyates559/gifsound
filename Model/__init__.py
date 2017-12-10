b3b2f19c4e42ff5d6c58f51300a0d57d1e110742
Carsen Yates <fatburgeriii@gmail.com>
2017-12-10 14:26:49 -0800

    Doc outline

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,5 +1,5 @@
 ## COURSE: CST 205 - Multimedia Design & Programming
 ## TITLE: __init__.py
-## ABSTRACT: This file is needed for Controller to be considered a Python Module
+## ABSTRACT: This file is needed for Model to be considered a Python Module
 ## AUTHORS: Erick Shaffer
 ## DATE: 
