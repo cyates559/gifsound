54162bcf5da96595dfe21f723a84c8d7444cad5c
Carsen Yates <fatburgeriii@gmail.com>
2017-12-10 14:17:37 -0800

    Added Documentation

--- youtube.py
+++ youtube.py
@@ -1,12 +1,19 @@
 #!pyenv/bin/python
 
-# AUTHOR: Carsen Yates
-# DATE: 11/15/2017
-# PURPOSE: To embed youtube videos like a pro
+## TITLE: youtube.py
+## COURSE: CST 205 - Multimedia Design & Programming
+## AUTHORS: Carsen Yates
+## DATE: 11/15/2017
+## ABSTRACT: This file parses YouTube Video URLs and extracts the Video ID
+## The Video ID can be further used to embed YouTube videos into webpages dynamically. 
 
 import urllib.parse
 
-
+# Takes a YouTube Video URL as input and returns the Video ID
+# URLs come in three forms:
+# 1) www.youtube.com/embed/<ID>
+# 2) www.youtube.com/watch?v=<ID>
+# 3) youtu.be/<ID>
 def get_video_id(main_url):
     vid = None
     # User gave us the url we are looking for, so we are done here.
