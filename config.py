753292c5a7272127e40fd3d7de6938dbdcbe8030
Carsen Yates <fatburgeriii@gmail.com>
2017-12-10 14:56:01 -0800

    Doc outline

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 #!pyenv/bin/python
+
+## COURSE: CST 205 - Multimedia Design & Programming
+## TITLE: config.py
+## ABSTRACT: This file parses the settings.yml file
+## AUTHORS: Erick Shaffer
+## DATE: 
 import yaml
 
 with open('settings.yml', 'r') as f:
