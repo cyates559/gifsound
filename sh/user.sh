6a1bde476c9b97705b9ba0daa7c608e95a8bc7c1
Carsen Yates <fatburgeriii@gmail.com>
2017-12-10 14:31:41 -0800

    Doc outline

--- sh/user.sh
+++ sh/user.sh
@@ -1,1 +1,7 @@
+## COURSE: CST 205 - Multimedia Design & Programming
+## TITLE: user.sh
+## ABSTRACT: 
+## AUTHORS: Erick Shaffer
+## DATE: 
+
 curl -X GET 127.0.0.1:5000/api/user_info/1/random_keyz
